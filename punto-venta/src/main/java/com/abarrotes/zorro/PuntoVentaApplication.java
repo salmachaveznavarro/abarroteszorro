26c70197d14641ab5b7bdb1cf9b659a91cf87d8e
salmachaveznavarro <salmachavez30@aragon.unam.mx>
2025-04-28 10:50:36 -0600

    Proyecto inicial
    Proyecto con dependecias de spring

---
+++ punto-venta/src/main/java/com/abarrotes/zorro/PuntoVentaApplication.java
@@ -0,0 +1,13 @@
+package com.abarrotes.zorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoVentaApplication.class, args);
+	}
+
+}
